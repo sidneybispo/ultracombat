3f892d5d05d411d592b90779e07ea84cbefefec5
sidneybispo <protecney@gmail.com>
2021-07-20 13:33:39 -0300

    classe luta iniciada

--- src/ultraemojicombat/Luta.java
+++ src/ultraemojicombat/Luta.java
@@ -2,6 +2,49 @@ package ultraemojicombat;
 // Autor: Sidney Bispo
 public class Luta {
     // Atributos
+    private Lutador desafiado;
+    private Lutador desafiante;
+    private int rounds;
+    private boolean aprovada;
+    // Métodos Públicos
+    public void marcarLuta(){
+        
+    }
+   public void lutar(){
+     
+    }
+    // Métodos Especiais
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
     
-    // Métodos
 }
